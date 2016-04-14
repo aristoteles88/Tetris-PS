d9c29d322286b67177c39f777e9767dc59b10be4
Pedro Araújo <pedrobucci@gmail.com>
2016-04-14 09:46:51 -0300

    Added files via upload

---
+++ src/tela_Teste.c
@@ -0,0 +1,79 @@
+#include <stdio.h>    // Primeiro as dependências do módulo que está sendo testado;
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ncurses.h>
+
+#include "tela.h" // Dependência do escopo que está sendo testado
+
+#include "CUnit/CUnit.h" // Aqui as bibliotecas CUnit necessárias para rodar o teste;
+#include "CUnit/Basic.h"
+
+#include "tela.c" // Este é o escopo que estará sobre julgo nesses testes
+
+
+//algumas funções do módulo não tiveram testes estáticos realizados porque seu retorno é vazio.
+
+void adicionar_suite(void);
+
+void  adicionar_suite(void){
+	CU_pSuite suite;
+	
+	/*Cria uma suite que conterá todos os testes*/
+	suite = CU_add_suite("Testes da smo_datas",NULL,NULL);
+	
+	
+	/*Adiciona os testes para a função DT_data_valida*/
+	CU_ADD_TEST(suite, Teste_da_criacao_de_Tela );
+
+}
+
+void Teste_da_criacao_de_Tela(void){
+/*Este teste verificará se a estrutura foi criada corretamente
+e também se seus valores foram preenchidos como esperado*/
+
+	int pontuacao, gameOver, pecaEncaixada;
+
+	Tela *tela;
+	tela  = malloc(sizeof(Tela));
+
+	tela = cria_tela(); // Primeira etapa para saber se retorna um tipo Tela
+
+	pontuacao = tela->cont_pontuacao;
+	gameOver = tela->Game_over;
+	pecaEncaixada = tela->peca_encaixada;
+
+	CU_ASSERT_TRUE( !pontuacao ); // verifica se Pontuação é igual a 0
+	CU_ASSERT_TRUE( !gameOver ); // verifica se Game Over é igual a 0
+	CU_ASSERT_TRUE( !pecaEncaixada ); // verifica se Peça Encaixada é igual a 0
+
+}
+
+
+
+
+int
+main(void){
+	/*Inicializa o registro de suítes e testes do CUnit*/	
+	if (CUE_SUCCESS != CU_initialize_registry())
+    		return CU_get_error();
+    
+    	/*Adiciona os testes ao registro*/ 	
+   	adicionar_suite();
+	
+	/*Muda o modo do CUnit para o modo VERBOSE
+	 O modo VERBOSE mostra algumas informacoes a
+	 mais na hora da execucao. Outras opções: NORMAL e SILENT*/
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	
+	/*Roda os testes e mostra na tela os resultados*/
+	CU_basic_run_tests();
+	//CU_console_run_tests();	
+	//CU_automated_run_tests();	
+
+
+	/*Limpa o registro*/
+	CU_cleanup_registry();
+
+	return CU_get_error();
+}
